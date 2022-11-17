804bdec5ceaf7ca8129a5d725d29700bbef3107c
Ayxan23 <ayxan042223@gmail.com>
2022-11-18 01:41:25 +0400

    c# method

---
+++ cmethod/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace cmethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(Calculator(12, 4, '*')); 
+
+        }
+
+        static int Calculator(int num1,int num2, char opr)
+        {
+            int result = 404;
+
+            if (opr == '+')
+            {
+                result = num1 + num2;
+            }
+            else if (opr == '-')
+            {
+                result = num1 - num2;
+            }
+            else if (opr == '/')
+            {
+                result = num1 / num2;
+            }
+            else if (opr == '*')
+            {
+                result = num1 * num2;
+            }
+            else
+            {
+                Console.Write("opr olaraq bunlardan istifade et: + , - , / , * , Error");
+            }
+
+            return result;
+        }
+    }
+}
